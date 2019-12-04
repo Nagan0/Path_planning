bde7d0f76e6d3a53527d07541f9bbab29710c040
NaganoShuntaro <q104084s@mail.kyutech.jp>
2019-12-05 04:05:23 +0900

    Update

--- src/ik_bulk.cpp
+++ src/ik_bulk.cpp
@@ -26,7 +26,7 @@ void callback(const geometry_msgs::PoseArray::ConstPtr& pose_array_msg)
     moveit::planning_interface::MoveGroupInterface move_group("arm");
 
     const int num_of_points = pose_array_msg->poses.size();
-    for(int j=0; j<num_of_points; j++)
+    for(int j=(num_of_points-1); j>=0; j--)
     {
         waypoints.push_back(pose_array_msg->poses[j]);
     }
@@ -48,7 +48,7 @@ void callback(const geometry_msgs::PoseArray::ConstPtr& pose_array_msg)
 	visual_tools.publishAxisLabeled(waypoints[i], "pt" + std::to_string(i), rviz_visual_tools::SMALL);
     }
     visual_tools.trigger();
-    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui");
+    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
 }
 
 int main(int argc, char** argv)
@@ -58,90 +58,9 @@ int main(int argc, char** argv)
     ros::AsyncSpinner spinner(1);
     spinner.start();
 
-    //moveit::planning_interface::MoveGroupInterface group("arm");
-   
-
     ros::Subscriber transformed_traj_sub = nh.subscribe<geometry_msgs::PoseArray>("transformed_traj", 10, callback);
     
 
-/*
-    ros::Subscriber transformed_traj_sub {nh.subscribe<geometry_msgs::PoseArray>("transformed_traj", 1,
-        std::function<void (const geometry_msgs::PoseArray::ConstPtr&)>
-	{
-            [&](const auto& target)
-            {	    
-	        std::cout << "=====Subscribed pose array=====" << std::endl;
-                group.setStartState(*group.getCurrentState());
-
-                const unsigned int num_of_points = target->poses.size();
-
-		robot_state::RobotState robot_state_goal(*group.getCurrentState());
-		const robot_state::JointModelGroup *joint_model_group = group.getCurrentState()->getJointModelGroup("arm");
-
-		moveit_msgs::TrajectoryConstraints initial_traj;
-		std::vector<moveit_msgs::Constraints> all_joint_constraints;
-
-                std::string joint_names[6] = {"joint_1","joint_2","joint_3","joint_4","joint_5","joint_6"};
-
-		std::map<std::string, double> joints;
-		std::map<std::string, double> init_joints;
-                bool ik;
-		bool is_first_point = true;
-
-		for(int j = 0; j < num_of_points; j++)
-		{
-		    std::cout << "===Now processing waypoint" << j << "===" << std::endl;
-                    std::vector<moveit_msgs::JointConstraint> joint_con_at_t;
-		    joint_model_group = group.getCurrentState()->getJointModelGroup("arm");
-
-		    std::cout << "[Position]" << std::endl;
-		    std::cout << "x: " << target->poses[j].position.x << std::endl;
-                    std::cout << "y: " << target->poses[j].position.y << std::endl;
-                    std::cout << "z: " << target->poses[j].position.z << std::endl;
-                    std::cout << "[Orientation]" << std::endl;
-		    std::cout << "w: " << target->poses[j].orientation.w << std::endl;
-		    std::cout << "x: " << target->poses[j].orientation.x << std::endl;
-		    std::cout << "y: " << target->poses[j].orientation.y << std::endl;
-		    std::cout << "z: " << target->poses[j].orientation.z << std::endl;
-
-
-                    ik = robot_state_goal.setFromIK(joint_model_group, target->poses[j], 10, 1);
-                    if(!ik)
-		    {
-		        ROS_ERROR_STREAM("Inverse kinematics could not obtain a valid joint angles.");
-		    }
-		    else
-		    {
-			for(int k = 0; k < 6; k++)
-		        {
-			    moveit_msgs::JointConstraint joint_con;
-			    joint_con.joint_name = joint_names[k];
-			    joint_con.position = robot_state_goal.getVariablePosition(joint_names[k]);
-			    joints[joint_names[k]] = robot_state_goal.getVariablePosition(joint_names[k]);
-			    joint_con.weight = 1.0;
-			    joint_con_at_t.push_back(joint_con);
-			}
-			if(is_first_point)
-		        {
-			    is_first_point = false;
-			    init_joints = joints;
-			}
-			moveit_msgs::Constraints con;
-			con.joint_constraints = joint_con_at_t;
-			all_joint_constraints.push_back(con);
-		    }
-
-		}
-
-
-		
-
-
-	    }
-	}
-
-    )};
-*/
 
     while(ros::ok())
     {
--- src/ik_solve.cpp
+++ src/ik_solve.cpp
@@ -12,109 +12,112 @@
 #include <moveit/move_group_interface/move_group.h>
 #include <moveit/robot_state/robot_state.h>
 
+void callback(const geometry_msgs::PoseArray::ConstPtr& pose_array_msg)
+{
+    moveit::planning_interface::MoveGroupInterface move_group("arm");
+    
+    //move_group.setStartState(*move_group.getCurrentState());
+    //robot_state::RobotState robot_state_goal(*move_group.getCurrentState());
+    const robot_state::JointModelGroup *joint_model_group = move_group.getCurrentState()->getJointModelGroup("arm");
+
+    moveit_msgs::TrajectoryConstraints initial_traj;
+    std::vector<moveit_msgs::Constraints> all_joint_constraints;
+
+    std::string joint_names[6] = {"joint_1","joint_2","joint_3","joint_4","joint_5","joint_6"};
+    
+    //std::map<std::string, double> joints;
+
+    std::cout << pose_array_msg->poses[0] << std::endl;
+    
+//---IK TEST---
+    geometry_msgs::Pose target;
+    target.orientation.w = -0.00022685;
+    target.position.x = 0.63565447;
+    target.position.y = 0.135733727;
+    target.position.z = 0.54390942;
+    target.orientation.x = -0.99999987;
+    target.orientation.y = -0.00008291;
+    target.orientation.z = -0.00045939;
+
+    move_group.setPoseTarget(target);
+    robot_state::RobotStatePtr robot_state_goal = move_group.getCurrentState();
+    //const robot_state::JointModelGroup *joint_model_group = move_group.getCurrentState()->getJointModelGroup("arm");
+    bool ik = robot_state_goal->setFromIK(joint_model_group, target, 4, 1);
+    std::cout << "bool ik = " << ik << std::endl;
+
+    std::map<std::string, double> joints;
+    joints["joint_1"] = robot_state_goal->getVariablePosition("joint_1");
+    joints["joint_2"] = robot_state_goal->getVariablePosition("joint_2");
+    joints["joint_3"] = robot_state_goal->getVariablePosition("joint_3");
+    joints["joint_4"] = robot_state_goal->getVariablePosition("joint_4");
+    joints["joint_5"] = robot_state_goal->getVariablePosition("joint_5");
+    joints["joint_6"] = robot_state_goal->getVariablePosition("joint_6");
+    move_group.setJointValueTarget(joints);
+
+    std::cout << "===Joint Angle===" << std::endl;
+    std::cout << "joint_1: "<< joints["joint_1"] << std::endl;
+    std::cout << "joint_2: "<< joints["joint_2"] << std::endl;
+    std::cout << "joint_3: "<< joints["joint_3"] << std::endl;
+    std::cout << "joint_4: "<< joints["joint_4"] << std::endl;
+    std::cout << "joint_5: "<< joints["joint_5"] << std::endl;
+    std::cout << "joint_6: "<< joints["joint_6"] << std::endl;
+
+    moveit::planning_interface::MoveGroup::Plan result_plan;
+    move_group.plan(result_plan);
+    move_group.move();
+
+
+//------
+
+
+
+/*
+    //for(int j=0; j<num_of_points; j++)
+    std::vector<moveit_msgs::JointConstraint> joint_con_at_t;
+    bool found_ik = robot_state_goal.setFromIK(joint_model_group, pose_array_msg->poses[0], 4,1);
+    if (found_ik)
+    {
+        for(int k=0; k<6; k++)
+	{
+	    moveit_msgs::JointConstraint joint_con;
+	    joint_con.joint_name = joint_names[k];
+	    joint_con.position = robot_state_goal.getVariablePosition(joint_names[k]);
+	    joints[joint_names[k]] = robot_state_goal.getVariablePosition(joint_names[k]);
+	    joint_con.weight = 1.0;
+	    joint_con_at_t.push_back(joint_con);
+
+	    std::cout << joints[joint_names[1]] << std::endl;
+	}
+    }else
+    {
+        ROS_ERROR_STREAM("Inverse kinematics could not obtain a valid joint angles.");
+    }
+   
+*/    
+}
+
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "ik_stomp_solver");
-  ros::NodeHandle nh;
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
+    ros::init(argc, argv, "ik_stomp_solver");
+    ros::NodeHandle nh;
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
 
-  moveit::planning_interface::MoveGroupInterface group("arm");
+    //moveit::planning_interface::MoveGroupInterface group("arm");
+    ros::Subscriber transformed_traj_sub = nh.subscribe<geometry_msgs::PoseArray>("transformed_traj", 1, callback);
+    //callback();
 
-  ros::Subscriber transformed_traj_sub {nh.subscribe<geometry_msgs::PoseArray>("transformed_traj", 1,
-    std::function<void (const geometry_msgs::PoseArray::ConstPtr&)>
+    while(ros::ok())
     {
-      [&](const auto& target)
-      {
-        std::cout << "=====SUBSCRIBED POSE ARRAY=====" << std::endl;
-        group.setStartState(*group.getCurrentState());
-
-        const unsigned int num_of_points = target->poses.size();
-
-        robot_state::RobotState robot_state_goal(*group.getCurrentState());
-        const robot_state::JointModelGroup *joint_model_group = group.getCurrentState()->getJointModelGroup("arm");
-
-        moveit_msgs::TrajectoryConstraints initial_traj;
-        std::vector<moveit_msgs::Constraints> all_joint_constraints;
-
-        std::string joint_names[6] = {"joint_1","joint_2","joint_3","joint_4", "joint_5","joint_6"};
-
-        std::map<std::string, double> joints;
-        std::map<std::string, double> init_joints;
-        bool ik;
-        bool is_first_point = true;
-
-        for(int j=0; j<num_of_points; j++)
-        {
-          std::cout << "Now processing waypoint " << j << std::endl;
-          std::vector<moveit_msgs::JointConstraint> joint_con_at_t;
-          joint_model_group = group.getCurrentState()->getJointModelGroup("arm");
-
-          std::cout << "===Target pose===" << std::endl;
-          std::cout << "[Orientation]" << std::endl;
-          std::cout << "w: " << target->poses[j].orientation.w << std::endl;
-          std::cout << "x: " << target->poses[j].orientation.x << std::endl;
-          std::cout << "y: " << target->poses[j].orientation.y << std::endl;
-          std::cout << "z: " << target->poses[j].orientation.z << std::endl;
-          std::cout << "[Position]" << std::endl;
-          std::cout << "x: " << target->poses[j].position.x << std::endl;
-          std::cout << "y: " << target->poses[j].position.y << std::endl;
-          std::cout << "z: " << target->poses[j].position.z << std::endl;
-          std::cout << "=================" << std::endl;
-
-          ik = robot_state_goal.setFromIK(joint_model_group, target->poses[j], 4, 1);
-          if(!ik)
-          {
-            ROS_ERROR_STREAM("Inverse kinematics could not obtain a valid joint angles.");
-          }
-          else
-          {
-            for(int k=0; k<6; k++)
-            {
-              moveit_msgs::JointConstraint joint_con;
-              joint_con.joint_name = joint_names[k];
-              joint_con.position = robot_state_goal.getVariablePosition(joint_names[k]);
-              joints[joint_names[k]] = robot_state_goal.getVariablePosition(joint_names[k]);
-              joint_con.weight = 1.0;
-              joint_con_at_t.push_back(joint_con);
-            }
-            if(is_first_point)
-            {
-              is_first_point = false;
-              init_joints = joints;
-            }
-            moveit_msgs::Constraints con;
-            con.joint_constraints = joint_con_at_t;
-            all_joint_constraints.push_back(con);
-          }
-        }
-
-        robot_state::RobotState robot_state_init(*group.getCurrentState());
-        group.setStartState(robot_state_init);
-        group.setJointValueTarget(init_joints);
-        moveit::planning_interface::MoveGroup::Plan init_plan;
-        group.plan(init_plan);
-        group.execute(init_plan);
-
-        initial_traj.constraints = all_joint_constraints;
-        robot_state::RobotState robot_state_start(*group.getCurrentState());
-        group.setStartState(robot_state_start);
-        group.setTrajectoryConstraints(initial_traj);
-        group.setJointValueTarget(joints);
-
-        moveit::planning_interface::MoveGroup::Plan result_plan;
-        group.plan(result_plan);
-        group.execute(result_plan);
-      }
+        ros::spinOnce();
     }
-  )};
 
-  while(ros::ok())
-  {
+/*    
     ros::spinOnce();
-  }
+    ros::shutdown();
+*/
 
-  return 0;
+    return 0;
 
 }
